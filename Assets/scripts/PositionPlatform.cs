b3fa25345c4fd3cda958250b357b0f40c2b04bea
Edgar Xolop <edgarxoix@gmail.com>
2015-11-15 13:05:47 -0600

    Selection of the pivot

--- Assets/scripts/PositionPlatform.cs
+++ Assets/scripts/PositionPlatform.cs
@@ -3,32 +3,69 @@ using System.Collections;
 
 public class PositionPlatform : MonoBehaviour
 {
-    public float rotationSpeed = 20f;
-    public float distance = 5f;
-    public Vector3 pivot = new Vector3(0f, 6.73f, 30f);
-    private float currentYAngle = 0f;
-    private float targetYAngle = 90f;
+
+	/***********************************************/
+    public int angleRotation = 90;
+	public float targetAngle = 0;
+	public bool isAnimed = false;
+	/***********************************************/
+
+	private float smooth = 20.0F;
+	private GameObject gPivot;
 
     // Use this for initialization
     void Start()
     {
+        gPivot = GameObject.FindWithTag("center");
     }
 
     // Update is called once per frame
     void Update()
-    {
-        currentYAngle = Mathf.MoveTowardsAngle(currentYAngle, targetYAngle, rotationSpeed * Time.deltaTime);
+	{
+		loadPivot();
 
-        /*transform.position = new Vector3(
-            pivot.x + Mathf.Sin(currentYAngle * Mathf.Deg2Rad) * distance,
-            transform.position.z,
-            pivot.z + Mathf.Cos(currentYAngle * Mathf.Deg2Rad) * distance
-        );*/
+		if (Input.GetKey("up") && !isAnimed) {
+			isAnimed =  true;
+		}
 
-        transform.position = new Vector3(
-            transform.position.x,
-            transform.position.y,
-             pivot.z + Mathf.Cos(currentYAngle * Mathf.Deg2Rad) * distance
-        );
+		if (isAnimed) {
+			transform.RotateAround (gPivot.transform.position, new Vector3 (0, 0, 1), smooth * Time.deltaTime);
+		}
+
+		targetAngle = transform.rotation.eulerAngles.z;
+
+		if (targetAngle <= (angleRotation + 1) && targetAngle >= (angleRotation - 1))
+		{
+			transform.RotateAround(gPivot.transform.position, Vector3.forward, 0);
+			if(targetAngle > 359){
+				angleRotation = 0;
+
+			}else{
+				angleRotation +=90; 
+			
+			}
+			isAnimed = false;
+		}
     }
+
+	void loadPivot()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			gPivot = GameObject.FindWithTag("top");
+
+		}else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+			gPivot = GameObject.FindWithTag("right");
+
+		}else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+			gPivot = GameObject.FindWithTag("bottom");
+
+		}else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+			gPivot = GameObject.FindWithTag("left");
+
+		}else if (Input.GetKeyDown(KeyCode.Alpha0)) {
+			gPivot = GameObject.FindWithTag("center");
+
+		}
+
+	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # UNNAMED
+
+###Chage pivot
+* Activate animation with the up key
+* Selection of the pivot, list of the keys mapped:
+	- 1 : Top pivot.
+	- 2 : Right pivot.
+	- 3 : Bottom pivot.
+	- 4 : Left pivot.
+	- 5 : Center pivot 
+
 ###First commit
 * Game 2D
 * C#
